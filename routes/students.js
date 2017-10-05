fefdd2876220a0cdf055a3f0ffd35b51e6a14088
precioustosin <precioustosin@hotmail.com>
2017-10-05 19:05:28 +0100

    Created logic to retrieve, create, update and delete records from the database

--- routes/students.js
+++ routes/students.js
@@ -1,30 +1,63 @@
 import express from 'express';
+import Student from '../models/students';
 
 const router = express.Router();
 
 /* GET all students records */
 router.get('/', (req, res) => {
-  res.send('GET all students route working');
+  Student.find({}, (err, students) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({ students });
+    }
+  });
 });
 
 /* Retrieve information about a single student */
 router.get('/:id', (req, res) => {
-  res.send('GET one student route working');
+  Student.findById(req.params.id, (err, foundStudent) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({ foundStudent });
+    }
+  });
 });
 
 /* Create new student record */
 router.post('/', (req, res) => {
-  res.send('POST-Create new student route working');
+  console.log(req.body);
+  Student.create(req.body, (err, newStudent) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({ newStudent });
+    }
+  });
 });
 
 /* Update student record */
 router.put('/:id', (req, res) => {
-  res.send('PUT - Update existing student route working');
+  Student.findByIdAndUpdate(req.params.id, req.body, (err, updatedStudent) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send({ updatedStudent });
+    }
+  });
 });
 
 /* delete a student record using it's id */
 router.delete('/:id', (req, res) => {
-  res.send('DELETE - Delete existing student route working');
+  Student.findByIdAndRemove(req.params.id, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('DELETED STUDENT RECORD');
+      res.send('DELETED STUDENT RECORD');
+    }
+  });
 });
 
 export default router;
