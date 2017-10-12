a9ba7097bab941b3fee11a4b48bbbf13ca9c08b4
precioustosin <precioustosin@hotmail.com>
2017-10-12 21:09:29 +0100

    added entry point for react app, installed eslint react plugin, modified eslint config file to lint .jsx files

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   // res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname, '..views/index.html'));
+  res.sendFile(path.join(__dirname, '../../views/index.html'));
 });
 
 // module.exports = router;
---
+++ source/index.jsx
@@ -0,0 +1,32 @@
+/* eslint import/no-extraneous-dependencies: 'off', class-methods-use-this: 'off' */
+
+import React, { Component, PropTypes } from 'react';
+import { render } from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <h1>ReactJS Blueprints Chapter 6 Webpack scaffold</h1>
+      <div>
+        To use:
+        <p>
+          1. Run <strong>npm i</strong> to install
+        </p>
+        <p>
+          2. Run <strong>npm start</strong> to run dev server
+        </p>
+        <p>
+          3. View results in <strong>http://localhost:8080/
+        </strong>
+        </p>
+        <p>
+          4. Success
+        </p>
+      </div>
+    </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
