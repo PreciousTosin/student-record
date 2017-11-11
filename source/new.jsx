a2a2eaa818cae98860d1b27147b9eb3b08c480fd
precioustosin <precioustosin@hotmail.com>
2017-11-11 05:54:04 +0100

    created new and view components

---
+++ source/new.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from  'react';
+
+class New extends Component {
+  render() {
+    return (
+      <h1>Fill to create a new record</h1>
+    );
+  }
+}
+export default New;
---
+++ source/view.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class View extends Component {
+  render() {
+    return (
+      <h1>View detailed information</h1>
+    );
+  }
+}
+
+export default View;
