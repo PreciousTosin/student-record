c634446d96d99bbc3394dc8ae9107f64572f2237
precioustosin <precioustosin@hotmail.com>
2017-11-11 12:42:48 +0100

    imported switch file, moved navbar in html to App component, passed in Router to be rendered

--- source/index.jsx
+++ source/index.jsx
@@ -4,32 +4,44 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
+
+import { BrowserRouter as Router,  Link } from 'react-router-dom';
+
+import SwitchRoute from './switch';
+
 import '../public/stylesheets/style.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-      <h1>ReactJS Blueprints Chapter 6 Webpack scaffold</h1>
-      <div>
-        To use:
-        <p>
-          1. Run <strong>npm i</strong> to install
-        </p>
-        <p>
-          2. Run <strong>npm start</strong> to run dev server
-        </p>
-        <p>
-          3. View results in <strong>http://localhost:8080/
-        </strong>
-        </p>
-        <p>
-          4. Success
-        </p>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="/">NORMAN</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item"><Link className="nav-link" to="/new">New Record</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/view">View Record</Link></li>
+            </ul>
+          </div>
+        </nav>
+
+        <div className="container">
+          <h1>ReactJS scaffold</h1>
+
+          <SwitchRoute/>
+        </div>
       </div>
-    </div>
     );
   }
 }
 
-render(<App />, document.getElementById('app'));
+render((
+  <Router>
+    <App/>
+  </Router>
+), document.getElementById('app'));
