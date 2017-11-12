1c475f5f04c750780cb02a52a7c28a4e2f21c89a
precioustosin <precioustosin@hotmail.com>
2017-11-12 16:16:55 +0100

    modified stident model by adding more definitions to the schema

--- models/students.js
+++ models/students.js
@@ -4,7 +4,13 @@ import mongoose from 'mongoose';
 const studentSchema = new mongoose.Schema({
   name: String,
   age: Number,
+  gender: String,
+  DOB: String,
+  phone: String,
+  email: String,
+  address: String,
   course: String,
+  grade: Number,
 });
 
 const Student = mongoose.model('Student', studentSchema);
