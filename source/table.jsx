76751c50c2f1464d67bd3e3ffedd2b73026f6fd2
precioustosin <precioustosin@hotmail.com>
2017-11-12 16:13:56 +0100

    created methods to format data, initialise table

--- source/table.jsx
+++ source/table.jsx
@@ -8,6 +8,42 @@ require('datatables.net-responsive-bs4');
 require('datatables.net-select');
 
 class Table extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      records: [],
+    }
+  }
+  _reFormat() {
+    const finalData = {};
+    const largerArr = [];
+
+    data.students.forEach((studentObj) => {
+      const studentArr = $.map(studentObj, el => el.toString());
+      largerArr.push(studentArr);
+    });
+
+    finalData.data = largerArr;
+    console.log(finalData);
+    // console.log(JSON.stringify(finalData));
+    // return JSON.stringify(finalData);
+    return finalData;
+  }
+  _getRecords() {
+    $.ajax({
+      url: '/students',
+    }).done((data) => {
+      console.log('RETRIEVED DATA');
+      console.log(data);
+      return this._reFormat(data);
+    });
+  }
+
+  _renderTable() {
+
+  }
+
   render() {
     return (
       <table id="my-table" className="display table table-striped table-hover" width="100%">
